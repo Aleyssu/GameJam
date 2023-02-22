75c2bc57d68f7326e9c7f9537382ba4c65e68317
DailySugar <alexsunn30@gmail.com>
2023-02-22 17:38:43 -0500

    Improved Movement Beta

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,10 +5,17 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-	public MovementData data;
+	public PlayerMovementData data;
     public Rigidbody2D rb;
+	public BoxCollider2D floorCollider;
+	public BoxCollider2D wallColliderRight;
+	public BoxCollider2D wallColliderLeft;
 	private float lastOnGroundTime = 1;
 	private float lastSinceJumpPress = 0;
+	private float jumpingTime = 0;
+	private bool isJumping = false;
+	private bool wallJumping = false;
+	private short wallJumpingDirection = 1;
 	private Vector2 moveInput;
 	public LayerMask groundLayer;
 
@@ -17,6 +24,7 @@ public class PlayerMovement : MonoBehaviour
 		// Jumping with input buffering and coyote time
         lastOnGroundTime += Time.deltaTime;
 		lastSinceJumpPress += Time.deltaTime;
+		jumpingTime += Time.deltaTime;
 		
 		if(isGrounded()) {
 			lastOnGroundTime = 0;
@@ -24,11 +32,34 @@ public class PlayerMovement : MonoBehaviour
 		if(Input.GetButtonDown("Jump")) {
 			lastSinceJumpPress = 0;
 		}
+		if(isJumping && ((!Input.GetButton("Jump")) || jumpingTime > data.jumpTime)) {
+			isJumping = false;
+			rb.gravityScale = data.gravityScale;
+		}
 
-		if(lastOnGroundTime < data.coyoteTime && lastSinceJumpPress < data.jumpBuffer) {
-			rb.velocity = new Vector2(rb.velocity.x, data.jumpVelocity);
-			lastSinceJumpPress = data.jumpBuffer;
-			lastOnGroundTime = data.coyoteTime;
+		// For controlling acceleration while wall jumping
+		if(wallJumping && jumpingTime > data.wallJumpTime) {
+			wallJumping = false;
+		}
+
+		// Regular jump
+		if(lastSinceJumpPress < data.jumpBuffer) {
+			if(lastOnGroundTime < data.coyoteTime) {
+				Jump();
+				lastSinceJumpPress = data.jumpBuffer;
+				lastOnGroundTime = data.coyoteTime;
+			}
+			// Wall jump
+			else if(wallColliderRight.IsTouchingLayers(LayerMask.GetMask("Floor"))) {
+				wallJumping = true;
+				wallJumpingDirection = -1;
+				Jump();
+			}
+			else if(wallColliderLeft.IsTouchingLayers(LayerMask.GetMask("Floor"))) {
+				wallJumping = true;
+				wallJumpingDirection = 1;
+				Jump();
+			}
 		}
 		moveInput.x = Input.GetAxisRaw("Horizontal");
     }
@@ -36,11 +67,11 @@ public class PlayerMovement : MonoBehaviour
     private void FixedUpdate()
 	{
 		// Movement - force applied is calculated by runForce * the difference in velocity between the current and max
-		if(lastOnGroundTime > 0.1f) {
-			rb.AddForce(new Vector2(data.airAccelMult * moveInput.x * data.runForce * Mathf.Abs(data.runMaxSpeed * moveInput.x - rb.velocity.x), 0));
-		}
-		else {
+		if(lastOnGroundTime <= 0.1f) {
 			rb.AddForce(new Vector2(moveInput.x * data.runForce * Mathf.Abs(data.runMaxSpeed * moveInput.x - rb.velocity.x), 0));
+		}
+		else if(!wallJumping) {
+			rb.AddForce(new Vector2(data.airAccelMult * moveInput.x * data.runForce * Mathf.Abs(data.runMaxSpeed * moveInput.x - rb.velocity.x), 0));
 		}
 
 		// Reduce speed when past the limit and running on ground (bhopping will preserve momentum)
@@ -49,12 +80,25 @@ public class PlayerMovement : MonoBehaviour
 		}
 
 		// Decelerate rapidly when not holding down movement keys
-		if(Mathf.Abs(moveInput.x) < 0.01f) {
+		if(Mathf.Abs(moveInput.x) < 0.01f && !wallJumping) {
 			rb.AddForce(new Vector2(-data.runDeccelForce * rb.velocity.x, 0));
 		}
     }
 
+	public void Jump() {
+		isJumping = true;
+		jumpingTime = 0;
+		lastSinceJumpPress = data.jumpBuffer;
+		rb.gravityScale = data.gravityScaleJumping;
+		if(wallJumping) {
+			rb.velocity = new Vector2(data.jumpVelocity * data.wallJumpMult * wallJumpingDirection, data.jumpVelocity);
+		}
+		else {
+			rb.velocity = new Vector2(rb.velocity.x, data.jumpVelocity);
+		}
+	}
+
 	public bool isGrounded() {
-		return rb.IsTouchingLayers(groundLayer);	
+		return floorCollider.IsTouchingLayers(LayerMask.GetMask("Floor"));
 	}
 }
