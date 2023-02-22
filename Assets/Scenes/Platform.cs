7b15c177c5b88a0076cee4cae482324d5af27754
Alan Teng <alan.teng2003@gmail.com>
2023-02-22 00:41:11 -0500

    Update 1: Implemented Companion and Platform

---
+++ Assets/Scenes/Platform.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Platform : MonoBehaviour
+{
+    public List<GameObject> objectStanding = new List<GameObject>();
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.tag == "Entity")
+        {
+            objectStanding.Add(col.gameObject);
+        }
+    }
+
+    private void OnCollisionExit(Collision col)
+    {
+        for (int i = 0; i < objectStanding.Count; i++)
+        {
+            if (col.gameObject == objectStanding[i])
+            {
+                objectStanding.RemoveAt(i);
+            }
+        }
+    }
+}
