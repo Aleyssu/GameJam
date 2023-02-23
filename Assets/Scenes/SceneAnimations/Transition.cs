79dbcc949d9451e55aaee484d5049f5acb991daf
ATNoName <65098676+ATNoName@users.noreply.github.com>
2023-02-23 18:54:32 -0500

    Add Main Menu and Screen Transitions

---
+++ Assets/Scenes/SceneAnimations/Transition.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Transition : MonoBehaviour
+{
+    public Animator animator;
+    public bool load;
+    public string next;
+    public float transitionDelayTime = 1.0f;
+    void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (load == true)
+        {
+            LoadLevel();
+        }
+    }
+    public void LoadLevel()
+    {
+        StartCoroutine(DelayLoadLevel(next));
+    }
+    IEnumerator DelayLoadLevel(string sceneName)
+    {
+        animator.SetTrigger("Load");
+        yield return new WaitForSeconds(transitionDelayTime);
+        SceneManager.LoadScene(sceneName);
+    }
+}
