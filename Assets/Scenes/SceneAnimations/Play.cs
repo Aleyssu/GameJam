8cd27c401c196e73eebfd732fc76fdc379e4cc9c
ATNoName <alan.teng2003@gmail.com>
2023-03-06 23:44:13 -0500

    Add project files.

---
+++ Assets/Scenes/SceneAnimations/Play.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Play : MonoBehaviour
+{
+    public Transition levelLoad;
+    public AudioSource src;
+    public void OnButtonPressed()
+    {
+        levelLoad.load = true;
+        src.Play();
+    }
+}
